39fa162107d03f90e525a6c7e8f6a56c64468242
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-21 17:32:58 +0100

    Add css module

--- 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,43 @@
 import React, { useState } from 'react';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
+
+
+// const FormControl = styled.div`
+// 	margin: 0.5rem 0;
+
+// 	& label {
+// 		font-weight: bold;
+// 		display: block;
+// 		margin-bottom: 0.5rem;
+// 	}
+
+// 	& input {
+// 		display: block;
+// 		width: 100%;
+// 		border: 1px solid #ccc;
+// 		font: inherit;
+// 		line-height: 1.5rem;
+// 		padding: 0 0.25rem;
+// 	}
+
+// 	& input:focus {
+// 		outline: none;
+// 		background: #fad0ec;
+// 		border-color: #8b005d;
+// 	}
+
+// 	&.invalid input {
+// 		border-color: red;
+// 		background: #ffd7d7;
+// 	}
+
+// 	&.invalid label {
+// 		color: red;
+// 	}
+// `;
 
 const CourseInput = (props) => {
 	const [enteredValue, setEnteredValue] = useState('');
@@ -27,7 +63,7 @@ const CourseInput = (props) => {
 
 	return (
 		<form onSubmit={formSubmitHandler}>
-			<div className='form-control'>
+			<div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
 				<label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
 				<input
 					style={{
@@ -46,6 +82,8 @@ const CourseInput = (props) => {
 export default CourseInput;
 
 // dynamiczne dodawanie klas:
-{/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}></div>
+{
+	/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}></div>
 
-i w css tworzymy klasę .form-control.invalid i odpowiednio stylujemy  */}
+i w css tworzymy klasę .form-control.invalid i odpowiednio stylujemy  */
+}
