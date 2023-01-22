a68a9a543d555a5e2b318df0fd132e3551a0b657
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-22 15:20:47 +0100

    Add form and styles

---
+++ 01-starting-project/src/components/Users/AddUser.js
@@ -0,0 +1,21 @@
+import { Card } from '../UI/Card';
+import styles from './AddUser.module.css'
+import { Button } from '../UI/Button';
+
+export const AddUser = (props) => {
+	const addUserHandler = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<Card className={styles.input}>
+			<form onSubmit={addUserHandler}>
+				<label htmlFor='username'>Username</label>
+				<input id='username' type='text' />
+				<label htmlFor='age'>Age (Years)</label>
+				<input id='age' type='number' />
+				<Button type='submit'>Add User</Button>
+			</form>
+		</Card>
+	);
+};
