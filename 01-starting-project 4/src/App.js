c1fa468e09907139f2ceae15cb060503898718d9
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-25 13:32:26 +0100

    Add animation and finish the project

--- 01-starting-project 4/src/App.js
+++ 01-starting-project 4/src/App.js
@@ -5,21 +5,20 @@ import { useState } from 'react';
 import { CartProvider } from './store/CartProvider';
 
 function App() {
-
 	const [cartIsShown, setCartIsShown] = useState(false);
 
 	const showCartHandler = () => {
 		setCartIsShown(true);
-	}
+	};
 
 	const hideCartHandler = () => {
 		setCartIsShown(false);
-	}
+	};
 
 	return (
 		<CartProvider>
-		{cartIsShown && <Cart onClose={hideCartHandler}/>}
-			<Header onShowCart={showCartHandler}/>
+			{cartIsShown && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
 			</main>
