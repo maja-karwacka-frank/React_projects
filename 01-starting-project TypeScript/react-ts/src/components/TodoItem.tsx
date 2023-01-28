0c89b0f287a6060e5dd0402d838ee947a862ec15
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-28 15:51:50 +0100

    Add react app & typescript

---
+++ 01-starting-project TypeScript/react-ts/src/components/TodoItem.tsx
@@ -0,0 +1,3 @@
+export const TodoItem: React.FC<{ text: string }> = (props) => {
+	return <li>{props.text}</li>;
+};
---
+++ 01-starting-project TypeScript/react-ts/src/models/todo.ts
@@ -0,0 +1,15 @@
+// export type Todo = {
+// 	id: string;
+// 	text: string;
+// };
+
+export class Todo {
+    id: string;
+    text: string;
+
+    constructor(todoText: string) {
+        this.text = todoText;
+        this.id = new Date().toISOString()
+    }
+}
+
