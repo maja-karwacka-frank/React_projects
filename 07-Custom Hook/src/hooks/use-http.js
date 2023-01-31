f656e5e723d23071289be83685d1dea1f4d98632
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-31 17:17:15 +0100

    Adjusting custom hook logic

--- 07-Custom Hook/src/hooks/use-http.js
+++ 07-Custom Hook/src/hooks/use-http.js
@@ -1,17 +1,17 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-export const useHttp = (requestConfig, applyData) => {
+export const useHttp = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const sendRequest = async () => {
+	const sendRequest = useCallback(async (requestConfig, applyData) => {
 		setIsLoading(true);
 		setError(null);
 		try {
 			const response = await fetch(requestConfig.url, {
-				method: requestConfig.method,
-				headers: requestConfig.headers,
-				body: JSON.stringify(requestConfig.body),
+				method: requestConfig.method ? requestConfig.method : 'GET',
+				headers: requestConfig.headers ? requestConfig.headers : {},
+				body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
 			});
 
 			if (!response.ok) {
@@ -25,7 +25,7 @@ export const useHttp = (requestConfig, applyData) => {
 			setError(err.message || 'Something went wrong!');
 		}
 		setIsLoading(false);
-	};
+	}, []);
 
 	return {
 		isLoading,
