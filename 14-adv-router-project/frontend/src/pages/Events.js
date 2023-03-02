57f23ebb4f36d0862020478fdf90674e3d32a234
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-03-02 18:51:47 +0100

    Add loader

--- 14-adv-router-project/frontend/src/pages/Events.js
+++ 14-adv-router-project/frontend/src/pages/Events.js
@@ -1,9 +1,29 @@
-import { useLoaderData } from 'react-router-dom';
-
+import { json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 export const EventsPage = () => {
-	const events = useLoaderData();
+	const data = useLoaderData();
+	const events = data.events;
+
+	// if (data.isError) {
+	// 	return <p>{data.message}</p>;
+	// }
 
 	return <EventsList events={events} />;
+};
+
+export const loader = async () => {
+	const response = await fetch('http://localhost:8080/events');
+
+	if (!response.ok) {
+		// return {isError: true, message: 'Could not fetch events.'};
+		// throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {status: 500}) ;
+		throw json({
+			message: 'Could not fetch events.'
+		}, {
+			status: 500
+		})
+	} else {
+		return response; // useLoaderData wspiera response
+	}
 };
