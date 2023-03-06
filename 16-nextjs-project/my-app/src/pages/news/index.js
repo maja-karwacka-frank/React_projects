316e6396ad3b481d2c24e0a5eeaf73c6e24d2965
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-03-06 09:22:01 +0100

    Add links

--- 16-nextjs-project/my-app/src/pages/news/index.js
+++ 16-nextjs-project/my-app/src/pages/news/index.js
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 const NewsPage = () => {
-    return <h1>The News Page</h1>
-}
+	return (
+		<Fragment>
+			<h1>The News Page</h1>
+			<ul>
+				<li>
+					<Link href='/news/nextjs-is-a-great-framework'>
+						NextJS Is A Great Framework
+					</Link>
+				</li>
+				<li>
+					<Link href='/news/something-else'>Something Else</Link>
+				</li>
+			</ul>
+		</Fragment>
+	);
+};
 
 export default NewsPage;
