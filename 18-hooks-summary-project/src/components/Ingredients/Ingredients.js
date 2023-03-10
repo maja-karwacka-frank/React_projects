7829d9468d31c9576148d7e07828c1ee2aa715fa
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-03-10 15:13:45 +0100

    Add http request

--- 18-hooks-summary-project/src/components/Ingredients/Ingredients.js
+++ 18-hooks-summary-project/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import IngredientsList from './IngredientList';
 
 function Ingredients() {
-  return (
-    <div className="App">
-      <IngredientForm />
+	const [userIngredients, setUserIngredients] = useState([]);
 
-      <section>
-        <Search />
-        {/* Need to add list here! */}
-      </section>
-    </div>
-  );
+	const addIngredientHandler = (ingredient) => {
+		fetch(
+			'https://react-project-b3b30-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(ingredient),
+				headers: { 'Content-Type': 'application/json' },
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((responseData) => {
+				setUserIngredients((prevIngredients) => [
+					...prevIngredients,
+					{ id: responseData.name, ...ingredient },
+				]);
+			});
+	};
+
+	const removeIngredientHandler = (ingradientId) => {
+		setUserIngredients((prevIngredients) =>
+			prevIngredients.filter((ingredient) => ingredient.id !== ingradientId)
+		);
+	};
+
+	return (
+		<div className='App'>
+			<IngredientForm onAddIngredient={addIngredientHandler} />
+
+			<section>
+				<Search />
+				<IngredientsList
+					ingredients={userIngredients}
+					onRemoveItem={removeIngredientHandler}
+				/>
+			</section>
+		</div>
+	);
 }
 
 export default Ingredients;
