a36421b7a273cdb504b4d6dd89aedbf8e4bfd78a
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-03-16 14:53:57 +0100

    Add obj, arr, enum, tuples

--- TypeScript/02-project/app.ts
+++ TypeScript/02-project/app.ts
@@ -1,15 +1,30 @@
-const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
-	const result = n1 + n2;
-	if (showResult) {
-		console.log(phrase + result);
-	} else {
-		return n1 + n2;
-	}
+const person: {
+	name: string;
+	age: number;
+	hobbies: string[];
+	role: [number, string];
+} = {
+	name: 'Maja',
+	age: 18,
+	hobbies: ['Sports', 'Cooking'],
+	role: [2, 'author'], // tuples/krotka, tablica 2 elementowa, 1el jest numeryczny, 2el jest opisem lub identyfikatorem
 };
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
 
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase());
+}
+
+enum Role {
+	Admin,
+	Read_only,
+	AUTHOR,
+}
+
+const person2 = {
+	role: Role.Admin,
+};
