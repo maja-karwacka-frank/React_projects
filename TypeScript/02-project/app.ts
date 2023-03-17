903a601654f51e15179773a956c2332941754257
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-03-17 12:44:33 +0100

    Add unknown & never type

--- TypeScript/02-project/app.ts
+++ TypeScript/02-project/app.ts
@@ -1,27 +1,15 @@
-//union types
+let userInput: unknown;
+let userName: string;
 
-const combine = (
-	input1: number | string,
-	input2: number | string,
-	resultConversion: 'as-number' | 'as-text' // literal types
-) => {
-	let result;
-	if (
-		(typeof input1 === 'number' && typeof input2 === 'number') ||
-		resultConversion === 'as-number'
-	) {
-		result = +input1 + +input2;
-	} else {
-		result = input1.toString() + input2.toString();
-	}
-	return result;
+userInput = 5;
+userInput = 'Maja';
+if (typeof userInput === 'string') {
+	userName = userInput;
+}
+
+// never - nigdy nic nie zwraca
+const generateError = (message: string, code: number): never => {
+	throw { message: message, errorCode: code };
 };
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedAges);
-
-const combinedNames = combine('Maja', 'Wojtek', 'as-text');
-console.log(combinedNames);
+generateError('An error ocurred!', 500);
