036691375805e40b08fea95d32281034262928a9
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-02-29 08:34:55 +0100

    feat: album api

--- Redux/media-app/src/components/AlbumsList.jsx
+++ Redux/media-app/src/components/AlbumsList.jsx
@@ -1,3 +1,33 @@
+import { useFetchAlbumsQuery } from '../store';
+import { Skeleton } from './Skeleton';
+import Button from './Button';
+import { ExpandablePanel } from './ExpandablePanel';
+
 export const AlbumsList = ({ user }) => {
-	return <div>Albums for {user.name}</div>;
+	const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+	let content;
+
+	if (isLoading) {
+		content = <Skeleton times={3} />;
+	} else if (error) {
+		content = <div>Error loading albums.</div>;
+	} else {
+		content = data.map((album) => {
+			const header = <div>{album.title}</div>;
+
+			return (
+				<ExpandablePanel key={album.id} header={header}>
+					List of photos in the album
+				</ExpandablePanel>
+			);
+		});
+	}
+
+	return (
+		<div>
+			<div>Albums for {user.name}</div>
+			<div>{content}</div>
+		</div>
+	);
 };
