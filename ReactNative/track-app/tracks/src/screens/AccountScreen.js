5bd4725823fe88e633eddb0003ba6c1c01a4a1a4
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-05-07 14:13:29 +0200

    feat: sign out logic

--- ReactNative/track-app/tracks/src/screens/AccountScreen.js
+++ ReactNative/track-app/tracks/src/screens/AccountScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const AccountScreen = () => {
+	const { signout } = useContext(AuthContext);
 	return (
 		<View style={styles.container}>
 			<Text style={{ fontSize: 48 }}>AccountScreen</Text>
+			<Spacer>
+				<Button title='Sign Out' onPress={signout} />
+			</Spacer>
 		</View>
 	);
 };
