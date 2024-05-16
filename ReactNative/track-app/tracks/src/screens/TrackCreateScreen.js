a6c99c5b1b996ad1995088e57923d389e34f0429
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2024-05-16 09:39:59 +0200

    feat: map view

--- ReactNative/track-app/tracks/src/screens/TrackCreateScreen.js
+++ ReactNative/track-app/tracks/src/screens/TrackCreateScreen.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { requestForegroundPermissionsAsync } from 'expo-location';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+	const [err, setErr] = useState(null);
+	const startWatching = async () => {
+		try {
+			const { granted } = await requestForegroundPermissionsAsync();
+			if (!granted) {
+				throw new Error('Location permission not granted');
+			}
+		} catch (e) {
+			setErr(e);
+		}
+	};
+
+	useEffect(() => {
+		startWatching();
+	}, []);
+
 	const insets = useSafeAreaInsets();
 	return (
 		<View
@@ -13,9 +32,11 @@ const TrackCreateScreen = () => {
 				paddingLeft: insets.left,
 				paddingRight: insets.right,
 			}}>
-			<Text style={{ fontSize: 48, textAlign: 'center' }}>
-				TrackCreate Screen
+			<Text h2 style={{ textAlign: 'center' }}>
+				Create a Track
 			</Text>
+			<Map />
+			{err ? <Text>Please enable location services</Text> : null}
 		</View>
 	);
 };
